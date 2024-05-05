11dfcd6baf76def60872c62b29d34a5eb27d23c9
zz0-0 <122845086+zz0-0@users.noreply.github.com>
2024-05-05 12:25:29 +1200

    fix the shellroute bug

--- lib/src/share/presentation/widget/adaptive_scaffold_widget.dart
+++ lib/src/share/presentation/widget/adaptive_scaffold_widget.dart
@@ -90,24 +90,14 @@ class _AdaptiveScaffoldWidgetState
     return LayoutBuilder(
       builder: (context, constraints) {
         if (context.layout.breakpoint > LayoutBreakpoint.sm) {
-          return Row(
-            children: [
-              if (context.layout.breakpoint > LayoutBreakpoint.sm) ...[
-                NavigationSideBar(
-                  selectedIndex: widget.navigationShell.currentIndex,
-                  onDestinationSelected: goBranch,
-                ),
-                const VerticalDivider(thickness: 1, width: 1),
-              ],
-              const Expanded(
-                child: Padding(
-                  padding: EdgeInsets.all(16),
-                ),
-              ),
-            ],
+          return NavigationSideBar(
+            body: widget.navigationShell,
+            selectedIndex: widget.navigationShell.currentIndex,
+            onDestinationSelected: goBranch,
           );
         } else {
           return NavigationBottomBar(
+            body: widget.navigationShell,
             selectedIndex: widget.navigationShell.currentIndex,
             onDestinationSelected: goBranch,
           );
@@ -120,35 +110,51 @@ class _AdaptiveScaffoldWidgetState
 class NavigationSideBar extends StatelessWidget {
   final int selectedIndex;
   final Function(int) onDestinationSelected;
+  final Widget body;
 
   const NavigationSideBar({
     super.key,
     required this.selectedIndex,
     required this.onDestinationSelected,
+    required this.body,
   });
+
   @override
   Widget build(BuildContext context) {
-    return NavigationRail(
-      selectedIndex: selectedIndex,
-      onDestinationSelected: onDestinationSelected,
-      labelType: NavigationRailLabelType.selected,
-      destinations: const [
-        NavigationRailDestination(
-          icon: Icon(Icons.favorite_border),
-          selectedIcon: Icon(Icons.favorite),
-          label: Text('First'),
-        ),
-        NavigationRailDestination(
-          icon: Icon(Icons.bookmark_border),
-          selectedIcon: Icon(Icons.book),
-          label: Text('Second'),
-        ),
-        NavigationRailDestination(
-          icon: Icon(Icons.star_border),
-          selectedIcon: Icon(Icons.star),
-          label: Text('Third'),
-        ),
-      ],
+    return Scaffold(
+      body: Row(
+        children: [
+          NavigationRail(
+            selectedIndex: selectedIndex,
+            onDestinationSelected: onDestinationSelected,
+            labelType: NavigationRailLabelType.selected,
+            destinations: const [
+              NavigationRailDestination(
+                icon: Icon(Icons.favorite_border),
+                selectedIcon: Icon(Icons.favorite),
+                label: Text('First'),
+              ),
+              NavigationRailDestination(
+                icon: Icon(Icons.bookmark_border),
+                selectedIcon: Icon(Icons.book),
+                label: Text('Second'),
+              ),
+              NavigationRailDestination(
+                icon: Icon(Icons.star_border),
+                selectedIcon: Icon(Icons.star),
+                label: Text('Third'),
+              ),
+            ],
+          ),
+          const VerticalDivider(thickness: 1, width: 1),
+          Expanded(
+            child: Padding(
+              padding: EdgeInsets.all(16),
+              child: body,
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
@@ -156,35 +162,40 @@ class NavigationSideBar extends StatelessWidget {
 class NavigationBottomBar extends StatelessWidget {
   final int selectedIndex;
   final Function(int) onDestinationSelected;
+  final Widget body;
 
   const NavigationBottomBar({
     super.key,
     required this.selectedIndex,
     required this.onDestinationSelected,
+    required this.body,
   });
 
   @override
   Widget build(BuildContext context) {
-    return BottomNavigationBar(
-      currentIndex: selectedIndex,
-      onTap: onDestinationSelected,
-      items: const [
-        BottomNavigationBarItem(
-          icon: Icon(Icons.favorite_border),
-          activeIcon: Icon(Icons.favorite),
-          label: 'First',
-        ),
-        BottomNavigationBarItem(
-          icon: Icon(Icons.bookmark_border),
-          activeIcon: Icon(Icons.book),
-          label: 'Second',
-        ),
-        BottomNavigationBarItem(
-          icon: Icon(Icons.star_border),
-          activeIcon: Icon(Icons.star),
-          label: 'Third',
-        ),
-      ],
+    return Scaffold(
+      body: body,
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: selectedIndex,
+        onTap: onDestinationSelected,
+        items: const [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.favorite_border),
+            activeIcon: Icon(Icons.favorite),
+            label: 'First',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.bookmark_border),
+            activeIcon: Icon(Icons.book),
+            label: 'Second',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.star_border),
+            activeIcon: Icon(Icons.star),
+            label: 'Third',
+          ),
+        ],
+      ),
     );
   }
 }
