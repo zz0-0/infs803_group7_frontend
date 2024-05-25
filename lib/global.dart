f0e0a048b2a3317c9e805a95a71c3bacbe52daed
zz0-0 <122845086+zz0-0@users.noreply.github.com>
2024-05-25 19:13:56 +1200

    change url back to azure

--- lib/global.dart
+++ lib/global.dart
@@ -2,10 +2,10 @@ import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 import 'package:infs803_group7_frontend/src/share/domain/model/token_manager.dart';
 import 'package:tmdb_api/tmdb_api.dart';
 
-// const String url =
-//     "https://rustwebapp.happywave-d1214138.australiaeast.azurecontainerapps.io";
+const String url =
+    "https://rustwebapp.happywave-d1214138.australiaeast.azurecontainerapps.io";
 
-const String url = "http://localhost:8000";
+// const String url = "http://localhost:8000";
 
 final TokenManager tokenManager = TokenManager();
 final tmdbWithCustomLogs = TMDB(
